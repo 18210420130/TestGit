b32033f57ada014765516229ad7b2471fce1737d
18210420130 <243616455@qq.com>
2017-01-13 09:35:39 +0800

    --fast

--- mygithub/app/src/main/java/com/mygithub/MainActivity.java
+++ mygithub/app/src/main/java/com/mygithub/MainActivity.java
@@ -2,9 +2,12 @@ package com.mygithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+     private TextView tv;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
