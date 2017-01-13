6dd97b0a5d3cba61b626a4d13aa0dd5f6c048fa7
18210420130 <243616455@qq.com>
2017-01-13 09:57:34 +0800

    --fast

--- mygithub/app/src/main/java/com/mygithub/MainActivity.java
+++ mygithub/app/src/main/java/com/mygithub/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn= (Button) findViewById(R.id.btn);
+        btn1= (Button) findViewById(R.id.btn1);
     }
 }
